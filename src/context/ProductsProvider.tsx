c22bcd16081637447bb7cd624aecc4f87139fbfc
Travis Hunsberger <tfhunsberger@gmail.com>
2023-05-14 16:22:56 -0400

    cart context created

--- src/context/ProductsProvider.tsx
+++ src/context/ProductsProvider.tsx
@@ -1,4 +1,6 @@
-//define shape of product
+import { ReactElement, createContext, useState } from "react"
+
+//define shape of single product
 export type ProductType = {
     sku: string
     name: string
@@ -23,3 +25,28 @@ const initState: ProductType[] = [
         "price": 29.99
     }
 ]
+
+//define shape of data that will be returned from context hook for product list
+export type UseProductsContextType = { products: ProductType[] }
+
+//define init state of context that will be used in provider component for product list context
+const initContextState: UseProductsContextType = { products: [] }
+
+//create new context for the product list
+const ProductsContext = createContext<UseProductsContextType>(initContextState);
+
+//define children type (needed in react18+)
+type ChildrenType = { children?: ReactElement | ReactElement[] }
+
+//create provider
+export const ProductsProvider = ({ children }: ChildrenType): ReactElement => {
+    const [products, setProducts] = useState<ProductType[]>(initState)
+
+    return (
+        <ProductsContext.Provider value = {{ products }}>
+            {children}
+        </ProductsContext.Provider>
+    )
+}
+
+export default ProductsContext
