c8733ed6ef6ec7efcc46d067beb491ce5fe59848
Travis Hunsberger <tfhunsberger@gmail.com>
2023-05-14 13:58:11 -0400

    products context created

---
+++ src/context/ProductsProvider.tsx
@@ -0,0 +1,25 @@
+//define shape of product
+export type ProductType = {
+    sku: string
+    name: string
+    price: number
+}
+
+//define init state
+const initState: ProductType[] = [
+    {
+        "sku": "item0001",
+        "name": "Widget",
+        "price": 9.99
+    },
+    {
+        "sku": "item0002",
+        "name": "Premium Widget",
+        "price": 19.99
+    },
+    {
+        "sku": "item0003",
+        "name": "Deluxe Widget",
+        "price": 29.99
+    }
+]
