30b5bb21f1ab2db06be6745955ec7652e538ad99
Travis Hunsberger <tfhunsberger@gmail.com>
2023-05-15 16:08:03 -0400

    product and productlist set up. products view is working

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { CartProvider } from './context/CartProvider.tsx'
+import { ProductsProvider } from './context/ProductsProvider.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+	<ProductsProvider>
+		<CartProvider>
+			<App />
+		</CartProvider>
+	</ProductsProvider>
   </React.StrictMode>,
 )
